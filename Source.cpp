65981d1e200e65b9f3c2665edff08e31c232f088
makkomboanna <92595015+makkomboanna@users.noreply.github.com>
2021-11-12 19:18:29 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,67 @@
+// Анна Стасюк П13121
+#include <iostream>
+using namespace std;
+int main() {
+	/*int i = 2;
+	while (i <= 100) {
+		cout << i << :' ';
+		i += 2;
+	}*/
+	/*int a;
+	cin >> a;
+	while (a > 0) {
+		cin >> a;
+	}*/
+	//2
+	/*int n, i;
+	cin >> n;
+	i = 1
+    while (i * i <= n) {
+		cout << i * i << ' ';
+		i++;
+
+	}*/
+	//3
+	/*int i, n;
+	cin >> n;
+	i = 2;
+	while (n % i != 0) {
+		i++;
+	}
+	cout << i << endl;*/
+	
+	// 4
+	/*int i, n;
+	cin >> n;
+	i = 2;
+	while (i <= n/2) {
+		if (n % i == 0) cout << i << endl;
+		i++;
+	}*/
+	// 5
+	/*int i = 2, n;
+	cin >> n;
+	while (i <= n) {
+		i *= 2;
+	}*/
+
+	//6
+	/*ouble x, y, ;
+	int k = 0;
+	cin >> x >> y;
+	while (x < y) {
+		x = x * 1.1;
+		k++
+	}
+	cout << k << endl;*/
+
+	//7
+	int a;
+	cin >> a;
+	while (a != 0) {
+		cout << a % 10 << ' ';
+		a = a / 10;
+	}
+
+	return 0;
+}
